dccbcbc7c1c4c3c679d5c20b5d544de7193b5811
CodeReacher <58017164+sree2694@users.noreply.github.com>
2024-12-03 12:42:40 +0530

    Components
    Component, StandAlone, Component Decorator,

---
+++ angular_18_learn/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,AddEmployeeComponent,EmployeeListComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular_18_learn';
+}
---
+++ angular_18_learn/src/app/components/employee-list/employee-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-list',
+  imports: [],
+  template: '<h1 class ="text-primary ">Hello From Admin</h1>',
+  styles: ['.primary {color: red}']
+})
+export class EmployeeListComponent {
+
+}
