1df72993939ba2dd67182561987889c02c071114
CodeReacher <58017164+sree2694@users.noreply.github.com>
2024-12-03 16:57:38 +0530

    Data Binding
    Oneway (ts to html, hrml to ts),
    Two way ( ngModel) ,
    Signal

--- angular_18_learn/src/app/app.component.ts
+++ angular_18_learn/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { DatabindingComponent } from './components/databinding/databinding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,AddEmployeeComponent,EmployeeListComponent],
+  imports: [RouterOutlet,AddEmployeeComponent,EmployeeListComponent,DatabindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular_18_learn/src/app/components/databinding/databinding.component.ts
@@ -0,0 +1,42 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-databinding',
+  imports: [FormsModule],
+  templateUrl: './databinding.component.html',
+  styleUrl: './databinding.component.css'
+})
+export class DatabindingComponent {
+
+  //string, number, boolean, data
+
+  courseName: string = "Angular 18";
+
+  stateName: string = "Telangana";
+
+  inputType="radio";
+
+  myClassName: string = "bg-primary"; 
+
+  rollNo: number = 123;
+
+  isIndian: boolean = true;
+
+  currentDate: Date = new Date();
+
+  firstName = signal("Sree J");
+
+  constructor(){
+
+  }
+
+  changeCourseName(){
+    this.courseName="React Js";
+    this.firstName.set("Raghava");
+  }
+
+  showAlert(message: string){
+    alert(message)
+  }
+}
