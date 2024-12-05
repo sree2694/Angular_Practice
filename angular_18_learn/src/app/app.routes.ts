37b0ef613cb3c31157f73aaf173247bec51fe0d7
CodeReacher <58017164+sree2694@users.noreply.github.com>
2024-12-05 13:46:05 +0530

    Routing

--- angular_18_learn/src/app/app.routes.ts
+++ angular_18_learn/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { DataBindingComponent } from './components/databinding/databinding.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { StructuralDirComponent } from './components/directive/structural-dir/structural-dir.component';
+import { AttributeDirectiveComponent } from './components/directive/attribute-directive/attribute-directive.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'add-emp',
+        component: AddEmployeeComponent
+    },
+    {
+        path:'data-binding',
+        component: DataBindingComponent
+    },
+    {
+        path:'emp-list',
+        component: EmployeeListComponent
+    },
+    {
+        path:'structural-dir',
+        component: StructuralDirComponent
+    },
+    {
+        path:'attributedirective',
+        component: AttributeDirectiveComponent
+    },
+];
