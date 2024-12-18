4576c9141289400cd37fba3c1856d836043578d7
CodeReacher <58017164+sree2694@users.noreply.github.com>
2024-12-18 12:18:55 +0530

    Signal

--- angular_18_learn/src/app/app.routes.ts
+++ angular_18_learn/src/app/app.routes.ts
@@ -18,6 +18,7 @@ import { NgcontainerComponent } from './components/directive/ngcontainer/ngconta
 import { ViewchildComponent } from './components/decorators/viewchild/viewchild.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './service/auth.guard';
+import { SignalComponent } from './components/signal/signal.component';
 
 export const routes: Routes = [
   //default route
@@ -38,6 +39,10 @@ export const routes: Routes = [
         path: 'add-emp',
         component: AddEmployeeComponent,
       },
+      {
+        path:'signal',
+        component: SignalComponent
+    },
       {
         path: 'data-binding',
         component: DataBindingComponent,
